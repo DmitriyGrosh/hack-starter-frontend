b51834f2fbd0e3c4b5ea061c88f4ccd45700aab5
Dmitriy Grosh <dmitriy.grosh@gmail.com>
2024-10-24 15:24:13 +0300

    add logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouterProvider, useNavigate} from "react-router-dom";
 import {Login} from "./pages/login/Login.tsx";
 import {Register} from "./pages/register/Register.tsx";
 import {ComponentType, FC} from "react";
+import {Button} from "@mui/material";
 
-const Root = () => (
-  <>
-    Root
-  </>
-)
+const Root = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    navigate("/login")
+  }
+
+  return (
+    <>
+      Root
+      <Button onClick={logout}>Logout</Button>
+    </>
+  )
+}
 
 interface IRoute {
   component: ComponentType<any>;
